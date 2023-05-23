a38ca1d5cf7e5022387547b7c99922aabad4d9a5
Sp3cTer <efat1531@gmail.com>
2023-05-23 22:26:43 +0600

    Finished order for user and improved User UI

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,14 +1,47 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../constants/color_constant.dart';
+import '../providers/orders_provider.dart';
+import 'package:provider/provider.dart';
+import '../widgets/main_drawer.dart';
+import '../widgets/order_item.dart';
 
 class OrderHistoryScreen extends StatelessWidget {
   static String routeName = '/home/orders';
   @override
   Widget build(BuildContext context) {
+    final orderData = Provider.of<OrderProvider>(context);
     return Scaffold(
-      body: Center(
-        child: Text('Orders'),
+      drawer: MainDrawer(),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 25, top: 25),
+            child: Text(
+              'Your Orders',
+              style: GoogleFonts.openSans(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: kBlackColor),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.only(top: 10, right: 25, left: 25),
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return OrderItemListViewBuilder(orderData.orders[index]);
+            },
+            itemCount: orderData.orders.length,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,152 @@
+import 'package:book_rental_service/constants/color_constant.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import '../models/order.dart';
+
+class OrderItemListViewBuilder extends StatefulWidget {
+  final OrderItem orderItem;
+  OrderItemListViewBuilder(this.orderItem);
+
+  @override
+  State<OrderItemListViewBuilder> createState() =>
+      _OrderItemListViewBuilderState();
+}
+
+class _OrderItemListViewBuilderState extends State<OrderItemListViewBuilder> {
+  var _expanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //margin: const EdgeInsets.all(10),
+      child: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ListTile(
+            minLeadingWidth: 10,
+            leading: Container(
+              height: double.infinity,
+              child: const Icon(
+                Icons.timer_sharp,
+                size: 30,
+                color: Color.fromRGBO(217, 146, 167, 1),
+              ),
+            ),
+            title: Text(
+              widget.orderItem.orderId,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.raleway(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: kBlackColor,
+              ),
+            ),
+            subtitle: Text(
+              DateFormat.yMMMMd('en_US').format(widget.orderItem.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                _expanded
+                    ? Icons.expand_less_rounded
+                    : Icons.expand_more_rounded,
+                size: 30,
+                color: Color.fromRGBO(217, 146, 167, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Column(
+              children: [
+                ListView(
+                  shrinkWrap: true,
+                  children: widget.orderItem.listOfBooks
+                      .map(
+                        (element) => Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 5,
+                          ),
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      element.title,
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    '${element.amount} TK',
+                                    style: GoogleFonts.openSans(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      color: kGreyColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const Divider(
+                  thickness: 0.5,
+                  color: kGreyColor,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Amount :',
+                        style: GoogleFonts.openSans(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '${widget.orderItem.amount} TK',
+                        style: GoogleFonts.openSans(
+                          fontSize: 15,
+                          color: Colors.blueGrey[600],
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
